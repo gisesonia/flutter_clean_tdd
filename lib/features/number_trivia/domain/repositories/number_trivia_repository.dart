68c74894a6c7d51f665ab592786314ddf22b5c23
Gisele <gisesonia@gmail.com>
2021-01-22 14:40:33 -0300

    Estrutura de testes

--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -2,6 +2,7 @@ import 'package:clean_architeture_tdd/core/error/failures.dart';
 import 'package:clean_architeture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:dartz/dartz.dart';
 
+//classe abstrata
 abstract class NumberTriviaRepository {
   //programação funcional, usa a falha e o sucesso na função com o pacote dartz
   Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
